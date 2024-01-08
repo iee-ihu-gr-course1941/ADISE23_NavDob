60787eeb9379d05f46fc23ff5e806176d2a092b8
DeanDobrou <cdob18@gmail.com>
2024-01-09 01:50:42 +0200

    Db config changes

--- config.php
+++ config.php
@@ -6,10 +6,31 @@ $user = 'it185404';
 $pass = '';
 $charset = 'utf8mb4';
 
+// Check if the host is 'users.iee.ihu.gr'
+if (gethostname() == 'users.iee.ihu.gr') {
+    $pdo = new PDO(
+        "mysql:host=" . $host . ";dbname=" . $db . ";charset=" . $charset,
+        $user,
+        $pass,
+        [
+            // Use a Unix socket for connection
+            PDO::MYSQL_ATTR_UNIX_SOCKET => '/home/student/it/2018/it185404/mysql/run/mysql.sock',
+        ]
+    );
+} else {
+    // For other hosts, use a traditional connection with a password
+    $pdo = new PDO(
+        "mysql:host=" . $host . ";dbname=" . $db . ";charset=" . $charset,
+        $user,
+        $pass
+    );
+}
+
 return [
     'host' => $host,
     'db' => $db,
     'user' => $user,
     'pass' => $pass,
     'charset' => $charset,
+    'pdo' => $pdo, // Return the PDO object for database operations
 ];
